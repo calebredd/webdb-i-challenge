08b7dfda7c52e2aface17f09587435b17d6a69a9
Caleb Redd <caleb.redd@yahoo.com>
2019-11-13 22:09:58 -0700

    CRUD operations complete

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -5,5 +5,21 @@ const db = knex(configOptions.development);
 function find() {
   return db("accounts");
 }
+function findById(id) {
+  return db("accounts").where({ id });
+}
+function insert(accountData) {
+  return db("accounts").insert(accountData);
+}
+function update(id, accountData) {
+  return db("accounts")
+    .where({ id })
+    .update(accountData);
+}
+function remove(id) {
+  return db("accounts")
+    .where({ id })
+    .del();
+}
 
-module.exports = { find };
+module.exports = { find, findById, insert, update, remove };
--- server.js
+++ server.js
@@ -6,10 +6,54 @@ const server = express();
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
+server.get("/api", (req, res) => {
   Accounts.find()
     .then(accounts => {
       res.status(200).json({ accounts });
+    })
+    .catch(() =>
+      res.status(500).json({ errorMessage: "Could not access database" })
+    );
+});
+server.get("/api/:id", (req, res) => {
+  const { id } = req.params;
+  Accounts.findById(id)
+    .then(account => {
+      res.status(200).json({ account });
+    })
+    .catch(() =>
+      res.status(500).json({ errorMessage: "Could not access database" })
+    );
+});
+
+server.post("/api", (req, res) => {
+  const accountData = req.body;
+  Accounts.insert(accountData)
+    .then(account => {
+      res.status(200).json({ account });
+    })
+    .catch(() =>
+      res.status(500).json({ errorMessage: "Could not access database" })
+    );
+});
+
+server.put("/api/:id", (req, res) => {
+  const { id } = req.params;
+  const accountData = req.body;
+  Accounts.update(id, accountData)
+    .then(account => {
+      res.status(200).json({ account });
+    })
+    .catch(() =>
+      res.status(500).json({ errorMessage: "Could not access database" })
+    );
+});
+
+server.delete("/api/:id", (req, res) => {
+  const { id } = req.params;
+  Accounts.remove(id)
+    .then(account => {
+      res.status(200).json({ account });
     })
     .catch(() =>
       res.status(500).json({ errorMessage: "Could not access database" })
